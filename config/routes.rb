ea349f0a1870e8fc1c783463bdd3499030fc7db3
Dominykas <rumsa.dominykas@gmail.com>
2019-10-28 00:58:43 +0100

    React-router config v3

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
       resources :owners
     end
   end
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  !request.xhr? && request.format.html?
 end
-get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-!request.xhr? && request.format.html?
-end 
